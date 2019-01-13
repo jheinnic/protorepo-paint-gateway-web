6e480b0e27dee061bcc7361939ef103f2c170f97
John Heinnickel` <jheinnic@gmail.com>
2019-01-12 17:15:43 -0800

    Auto Creation of Proto

---
+++ ProtoServiceClientPb.ts
@@ -0,0 +1,77 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for name.jchein.portfolio.random_art
+ * @enhanceable
+ * @public
+ */
+
+// GENERATED CODE -- DO NOT EDIT!
+
+
+import * as grpcWeb from 'grpc-web';
+import {
+  CancelPaintArtworkTask,
+  CommandReceived,
+  CreatePaintArtworkTask,
+  InformationUpdate,
+  PercentDoneUpdate,
+  RetryableErrorUpdate,
+  UpdatePaintArtworkTask} from './proto_pb';
+
+export class PaintGatewayClient {
+  client_: grpcWeb.AbstractClientBase;
+  hostname_: string;
+  credentials_: null | { [index: string]: string; };
+  options_: null | { [index: string]: string; };
+
+  constructor (hostname: string,
+               credentials: null | { [index: string]: string; },
+               options: null | { [index: string]: string; }) {
+    if (!options) options = {};
+    options['format'] = 'text';
+
+    this.client_ = new grpcWeb.GrpcWebClientBase(options);
+    this.hostname_ = hostname;
+    this.credentials_ = credentials;
+    this.options_ = options;
+  }
+
+  methodInfocreatePaintArtworkTask = new grpcWeb.AbstractClientBase.MethodInfo(
+    CommandReceived,
+    (request: CreatePaintArtworkTask) => {
+      return request.serializeBinary();
+    },
+    CommandReceived.deserializeBinary
+  );
+
+  createPaintArtworkTask(
+    request: CreatePaintArtworkTask,
+    metadata: grpcWeb.Metadata) {
+    return this.client_.serverStreaming(
+      this.hostname_ +
+        '/name.jchein.portfolio.random_art.PaintGateway/createPaintArtworkTask',
+      request,
+      metadata,
+      this.methodInfocreatePaintArtworkTask);
+  }
+
+  methodInfocancelPaintArtworkTask = new grpcWeb.AbstractClientBase.MethodInfo(
+    CommandReceived,
+    (request: CancelPaintArtworkTask) => {
+      return request.serializeBinary();
+    },
+    CommandReceived.deserializeBinary
+  );
+
+  cancelPaintArtworkTask(
+    request: CancelPaintArtworkTask,
+    metadata: grpcWeb.Metadata) {
+    return this.client_.serverStreaming(
+      this.hostname_ +
+        '/name.jchein.portfolio.random_art.PaintGateway/cancelPaintArtworkTask',
+      request,
+      metadata,
+      this.methodInfocancelPaintArtworkTask);
+  }
+
+}
+
